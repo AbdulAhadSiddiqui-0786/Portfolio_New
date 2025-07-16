196086606b9e2ae178d13db6e4faa05ad63dec5a
AbdulAhadSiddiqui-0786 <abdulahadsiddiqui23@gmail.com>
2025-07-16 23:43:04 +0530

    my final portfolio commit

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -21,7 +21,13 @@ const ContactUs= () => {
       setHasSubmitted(true);
     }
   }, []);
-
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
 
